80942c464beb1afdf45361ba4757294b267f83b6
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-10-07 21:59:00 +1000

    Update GlobalEvents.cs

--- src/SharpIDE.Application/Features/Events/GlobalEvents.cs
+++ src/SharpIDE.Application/Features/Events/GlobalEvents.cs
@@ -6,11 +6,11 @@ namespace SharpIDE.Application.Features.Events;
 public class GlobalEvents
 {
 	public static GlobalEvents Instance { get; set; } = null!;
-	public EventWrapper<Task> ProjectsRunningChanged { get; private set; } = new(() => Task.CompletedTask);
-	public EventWrapper<Task> StartedRunningProject { get; private set; } = new(() => Task.CompletedTask);
-	public EventWrapper<SharpIdeProjectModel, Task> ProjectStartedDebugging { get; private set; } = new(_ => Task.CompletedTask);
-	public EventWrapper<SharpIdeProjectModel, Task> ProjectStoppedDebugging { get; private set; } = new(_ => Task.CompletedTask);
-	public EventWrapper<SharpIdeProjectModel, Task> ProjectStartedRunning { get; private set; } = new(_ => Task.CompletedTask);
-	public EventWrapper<SharpIdeProjectModel, Task> ProjectStoppedRunning { get; private set; } = new(_ => Task.CompletedTask);
-	public EventWrapper<ExecutionStopInfo, Task> DebuggerExecutionStopped { get; private set; } = new(_ => Task.CompletedTask);
+	public EventWrapper<Task> ProjectsRunningChanged { get; } = new(() => Task.CompletedTask);
+	public EventWrapper<Task> StartedRunningProject { get; } = new(() => Task.CompletedTask);
+	public EventWrapper<SharpIdeProjectModel, Task> ProjectStartedDebugging { get; } = new(_ => Task.CompletedTask);
+	public EventWrapper<SharpIdeProjectModel, Task> ProjectStoppedDebugging { get; } = new(_ => Task.CompletedTask);
+	public EventWrapper<SharpIdeProjectModel, Task> ProjectStartedRunning { get; } = new(_ => Task.CompletedTask);
+	public EventWrapper<SharpIdeProjectModel, Task> ProjectStoppedRunning { get; } = new(_ => Task.CompletedTask);
+	public EventWrapper<ExecutionStopInfo, Task> DebuggerExecutionStopped { get; } = new(_ => Task.CompletedTask);
 }
