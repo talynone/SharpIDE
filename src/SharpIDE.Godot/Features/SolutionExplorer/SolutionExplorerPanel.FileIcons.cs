631e11e50d94d9f218b5e9805a3308ddcce8bf36
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-11-28 23:37:07 +1000

    add json icon

--- src/SharpIDE.Godot/Features/SolutionExplorer/SolutionExplorerPanel.FileIcons.cs
+++ src/SharpIDE.Godot/Features/SolutionExplorer/SolutionExplorerPanel.FileIcons.cs
@@ -7,6 +7,7 @@ public partial class SolutionExplorerPanel
 {
     private readonly Texture2D _csIcon = ResourceLoader.Load<Texture2D>("uid://do0edciarrnp0");
     private readonly Texture2D _razorIcon = ResourceLoader.Load<Texture2D>("uid://cff7jlvj2tlg2");
+    private readonly Texture2D _jsonIcon = ResourceLoader.Load<Texture2D>("uid://csrwpjk77r731");
 
     private Texture2D GetIconForFileExtension(string fileExtension)
     {
@@ -14,6 +15,7 @@ public partial class SolutionExplorerPanel
         {
             ".cs" => _csIcon,
             ".razor" or ".cshtml" => _razorIcon,
+            ".json" => _jsonIcon,
             _ => _csIcon
         };    
         return texture;
